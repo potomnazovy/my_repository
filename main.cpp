c83462d4a6ccc8d4db50cb9c07a30bc3eda08967
potomnazovy <skeleton39876@mail.ru>
2025-10-25 12:50:34 +0300

    add count

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <cstddef>
 int main()
 {
   using u_t = unsigned;
   u_t a = 0;
-  std::cin >> a;
+  size_t count = 0;
+  while (std::cin >> a) {
+    count++;
+  }
   if (std::cin.eof()) {
-    std::cout << 0;
+    std::cout << count;
     std::cout << "\n";
   }
   else if (std::cin.fail()) {
@@ -13,3 +17,4 @@ int main()
     return 1;
   }
 }
+
