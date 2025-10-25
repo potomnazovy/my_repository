2e5ea065148474784349a68ce056211d93c1c214
potomnazovy <skeleton39876@mail.ru>
2025-10-25 17:32:56 +0300

    check is_pyth

--- main.cpp
+++ main.cpp
@@ -49,6 +49,44 @@ bool is_upper_mult_overflow(unsigned int res, unsigned int a, unsigned int b)
 {
   return (!any_zeros(a, b) && a > 0 && b > 0 && res == 0);
 }
+unsigned int is_pyth(unsigned int a, unsigned int b, unsigned int c)
+{
+  unsigned int a2 = multiplies(a, a);
+  unsigned int b2 = multiplies(b, b);
+  unsigned int c2 = multiplies(c, c);
+  if (is_upper_mult_overflow(a2, a, a))
+{
+  return 2;
+}
+  if (is_upper_mult_overflow(b2, b, b))
+{
+  return 2;
+}
+  if (is_upper_mult_overflow(c2, c, c))
+{
+  return 2;
+}
+  unsigned int k = sum(a2, b2);
+  unsigned int f = sum(a2, c2);
+  unsigned int g = sum(b2, c2);
+  if (is_upper_overflow(k, a2, b2))
+{
+  return 2;
+}
+  if (is_upper_overflow(f, a2, c2))
+{
+  return 2;
+}
+  if (is_upper_overflow(g, b2, c2))
+{
+  return 2;
+}
+  if (a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2)
+{
+  return 3;
+}
+  return 0;
+}
 bool isPyth(unsigned int a, unsigned int b, unsigned int c) {
     bool p = a * a == (b * b + c * c);
     p = p || (b * b == (a * a + c * c));
