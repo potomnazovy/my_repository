73181a6cf082bbdcae7cd5bbf93e9f336b65bfe0
potomnazovy <skeleton39876@mail.ru>
2025-10-25 13:10:27 +0300

    isPyth

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <cstddef>
+bool isPyth(unsigned int a, unsigned int b, unsigned int c) {
+    bool p = a * a == (b * b + c * c);
+    p = p || (b * b == (a * a + c * c));
+    p = p || (c * c == (a * a + b * b));
+    return c;
+}
 int main()
 {
   using u_t = unsigned;
-  u_t a = 0;
+  u_t a = 0, b = 0, c = 0;
   size_t count = 0;
   while (std::cin >> a) {
-    count++;
+    count += isPyth(a, b, c) ? 1 : 0;
+    c = b;
+    b = a;
   }
   if (std::cin.eof()) {
     std::cout << count;
