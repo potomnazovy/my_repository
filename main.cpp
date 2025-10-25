81c51d58b9f4f85940727c220fbba96fda3604af
potomnazovy <skeleton39876@mail.ru>
2025-10-25 12:36:32 +0300

    Input one uint

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
-int main() {
-    
+#include <iostream>
+int main()
+{
+  using u_t = unsigned;
+  u_t a = 0;
+  std::cin >> a;
+  if (std::cin.eof()) {
+    std::cout << 0;
+    std::cout << "\n";
+  }
+  else if (std::cin.fail()) {
+    std::cerr << "Error\n";
+    return 1;
+  }
 }
